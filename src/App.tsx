dc18281127b9327b8289e774557eb96c09e564c5
tateno <gd28.c.tateno@gmail.com>
2022-07-24 15:40:36 +0900

    add pokemon api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Counter } from "./components/counter";
+import { useGetPokemonByNameQuery } from "./api/pokemon";
 
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+
   return (
     <div>
       <h1>hello</h1>
       <Counter />
+
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
     </div>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { pokemonApi } from "../api/pokemon";
 import counterReducer from "../components/counter/slice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
